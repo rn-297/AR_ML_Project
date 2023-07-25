7d3b296525310b8663f067333e730b43a97404a2
rn-297 <rohan.warang297@gmail.com>
2023-07-25 22:40:52 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/aryan/ar_project/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.aryan.ar_project
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/ml_view_page.dart
@@ -0,0 +1,85 @@
+import 'package:camera/camera.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:google_mlkit_pose_detection/google_mlkit_pose_detection.dart';
+
+import 'detector_view.dart';
+import 'painter/pose_painter.dart';
+
+class PoseDetectorView extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _PoseDetectorViewState();
+}
+
+class _PoseDetectorViewState extends State<PoseDetectorView> {
+  final PoseDetector _poseDetector =
+  PoseDetector(options: PoseDetectorOptions());
+  bool _canProcess = true;
+  bool _isBusy = false;
+  CustomPaint? _customPaint;
+  String? _text;
+  var _cameraLensDirection = CameraLensDirection.back;
+
+  @override
+  void dispose() async {
+    _canProcess = false;
+    _poseDetector.close();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return DetectorView(
+      title: 'Pose Detector',
+      customPaint: _customPaint,
+      text: _text,
+      onImage: _processImage,
+      initialCameraLensDirection: _cameraLensDirection,
+      onCameraLensDirectionChanged: (value) => _cameraLensDirection = value,
+    );
+  }
+
+  Future<void> _processImage(InputImage inputImage) async {
+    if (!_canProcess) return;
+    if (_isBusy) return;
+    _isBusy = true;
+    setState(() {
+      _text = '';
+    });
+    final poses = await _poseDetector.processImage(inputImage);
+    if (poses.isNotEmpty) {
+      poses.first.landmarks.values.forEach((element) {
+        if(element.type==PoseLandmarkType.leftAnkle){
+          print("1 ${element.likelihood} ${element.x} ${element.y} ${element.z}");
+        }
+        if(element.type==PoseLandmarkType.rightAnkle){
+          print("2 ${element.likelihood} ${element.x} ${element.y} ${element.z}");
+        }
+        if(element.type==PoseLandmarkType.rightFootIndex){
+          print("3 ${element.likelihood} ${element.x} ${element.y} ${element.z}");
+        }
+        if(element.type==PoseLandmarkType.leftFootIndex){
+          print("4 ${element.likelihood} ${element.x} ${element.y} ${element.z}");
+        }
+      });
+    }
+    if (inputImage.metadata?.size != null &&
+        inputImage.metadata?.rotation != null) {
+      final painter = PosePainter(
+        poses,
+        inputImage.metadata!.size,
+        inputImage.metadata!.rotation,
+        _cameraLensDirection,
+      );
+      _customPaint = CustomPaint(painter: painter);
+    } else {
+      _text = 'Poses found: ${poses.length}\n\n';
+      // TODO: set _customPaint to draw landmarks on top of image
+      _customPaint = null;
+    }
+    _isBusy = false;
+    if (mounted) {
+      setState(() {});
+    }
+  }
+}
